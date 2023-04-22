efea4fd8ce064d2c913216fbe4942bc5ea6cba56
Thomas-Tomo <>
2023-04-22 10:02:28 +0000

    Add medium and hard words lists

--- hangman_art_words.py
+++ hangman_art_words.py
@@ -1,1 +1,25 @@
 easy_words = ['Pizza', 'Apple', 'Lemon', 'Beach', 'Happy', 'Party', 'Money', 'Rabbit', 'Tiger', 'Music']
+
+medium_words = [
+    'Bicycle',
+    'Elephant',
+    'Hospital',
+    'Rainbow',
+    'Diamond',
+    'Festival',
+    'Computer',
+    'Mountain',
+    'Painting',
+    'Balloon']
+
+hard_words = [
+    'Encyclopedia',
+    'Extravaganza',
+    'Entrepreneur',
+    'Accomplished',
+    'Phenomenally',
+    'Sophisticated',
+    'International',
+    'Remuneration',
+    'Satisfactory',
+    'Stereoscopic']
