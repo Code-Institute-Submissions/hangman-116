8a2fa86d44b1d1c0c95f286c664849942fa8f930
Thomas-Tomo <>
2023-04-22 10:15:32 +0000

    Update rules function with rules and skip option

--- run.py
+++ run.py
@@ -19,6 +19,15 @@ def rules():
     """
     show_rules = input("Do you want to display the rules? (y/n): \n")
 
-    
+    while show_rules.lower() not in ["y", "n"]:
+        show_rules = input("Invalid input. Please enter 'y' to display the rules or 'n' to skip: \n")
+
+    if show_rules.lower() == "y":
+        print("First, you'll need to pick a name to play with.")
+        print("Next, you'll choose a difficulty level: easy, medium, or hard.")
+        print("The game will choose a word, and your job is to guess the letters in that word.")
+        print("If you guess a letter correctly, it will be revealed in the word.")
+        print("But if you guess incorrectly, part of a drawing of a 'hangman' will appear.")
+        print("You win the game if you guess all the letters in the word before the hangman is fully drawn.")
 
 rules()
