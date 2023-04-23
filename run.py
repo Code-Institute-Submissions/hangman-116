c0ca252ea2c365cb86ac6a94d61cdcd717cb61ea
Thomas-Tomo <>
2023-04-23 21:01:58 +0000

    Fix line too long problems

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ print(
 
 print("Welcome to Hangman!")
 
+
 def rules():
     """
     Displays rules of the game if user inputs 'y', otherwise does nothing.
@@ -23,32 +24,35 @@ def rules():
     show_rules = input("Do you want to display the rules? (y/n): \n")
 
     while show_rules.lower() not in ["y", "n"]:
-        show_rules = input("Invalid input. Please enter 'y' to display the rules or 'n' to skip: \n")
+        show_rules = input("Enter 'y' to show rules or 'n' to skip: \n")
 
     if show_rules.lower() == "y":
-        print("First, you'll need to pick a name to play with.")
-        print("Next, you'll choose a difficulty level: easy, medium, or hard.")
-        print("The game will choose a word, and your job is to guess the letters in that word.")
-        print("If you guess a letter correctly, it will be revealed in the word.")
-        print("But if you guess incorrectly, part of a drawing of a 'hangman' will appear.")
-        print("You win the game if you guess all the letters in the word before the hangman is fully drawn.")
+        print("Pick a name to play with.")
+        print("Choose a difficulty level (easy, medium, or hard).")
+        print("Guess the letters in the chosen word.")
+        print("Correct guess reveals letter; incorrect guess adds to hangman.")
+        print("Win by guessing all letters before hangman is complete.")
 
 
 def create_username():
     """
-    Prompts the user to enter a username containing only letters and numbers, and returns it.
+    Prompts the user to enter a username containing only letters and numbers,
+    and returns it.
     """
     while True:
         try:
             name = input("Enter your name please: \n")
-            if not name.strip(): # Check if the input is empty or only contains whitespace
+            # Check if the input is empty or only contains whitespace
+            if not name.strip(): 
                 raise ValueError("Please enter a valid username.")
-            if not name.isalnum(): # Check if the input contains only letters and numbers
-                raise ValueError("Username must contain only letters and numbers.")
-        except ValueError as e: # Catch any ValueError exception and print the error message
+            # Check if the input contains only letters and numbers
+            if not name.isalnum(): 
+                raise ValueError("Username must use only letters and numbers.")
+        # Catch any ValueError exception and print the error message
+        except ValueError as e: 
             print(f"{e}")
-        else: # If no exception is raised, print a message and return the name
-            print(f"You have been condemned to death by hanging {name.upper()}!")
+        else:  # If no exception is raised, print a message and return the name
+            print(f"The noose awaits you {name.upper()}!")
             return name
 
 
