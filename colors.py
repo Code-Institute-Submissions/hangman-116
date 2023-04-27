022e79807169d62539e2c3211e78efbb299b51fd
Thomas-Tomo <>
2023-04-27 17:27:24 +0000

    Add docstrings, comments to colors.py

--- colors.py
+++ colors.py
@@ -1,13 +1,20 @@
 class Color:
+    """
+    A class representing a color in RGB format.
+    """
     def __init__(self, r, g, b):
         self.r = r
         self.g = g
         self.b = b
 
     def format(self, string):
+        """
+        Return the given string formatted with the RGB color values.
+        """
         return f"\033[38;2;{self.r};{self.g};{self.b}m{string}\033[0m"
 
 
+# Define a dictionary of color objects for convenience
 col = {
     "dark_orange": Color(255, 145, 0),
     "orange_red": Color(255, 69, 0),
