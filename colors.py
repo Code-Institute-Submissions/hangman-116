f04ac67ff671e093dc3dcc7bf13521c524e1c74a
Thomas-Tomo <>
2023-04-27 16:36:12 +0000

    Add object col that stores RGB colors

--- colors.py
+++ colors.py
@@ -6,3 +6,11 @@ class Color:
 
     def format(self, string):
         return f"\033[38;2;{self.r};{self.g};{self.b}m{string}\033[0m"
+
+
+col = {
+    "dark_orange": Color(255, 145, 0),
+    "orange_red": Color(255, 69, 0),
+    "red": Color(255, 0, 0),
+    "green": Color(0, 255, 0)
+}
